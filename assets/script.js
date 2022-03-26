e93d30335332f69526d586d53ebb0da0c9b21e1a
LittleToes <kristina.r.brennan@gmail.com>
2022-03-25 21:13:05 -0500

    displays question text and question buttons, adds event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,87 @@
 // global vars
 var startBtn = document.querySelector("#start-quiz");
 var startTime = 5;
+//don't forget to set time back to 75 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 var time = startTime;
 var timer = document.querySelector('#count-down');
+var questionCounter = 0;
+var questionDisplay = document.querySelector('#question');
+var answersBox = document.querySelector('#answers');
+var introP = document.querySelector('#intro');
 var allQuestions = [
-    {
-        question: "What's up?",
-        A: "nm",
-        B: "sup",
-        C: "I try my best"
-    },
-    {
-        question: "How r u",
-        A: "good",
-        B: "bad",
-        C: "great"
-    }
+  {
+      question: "What's up?",
+      A: "nm",
+      B: "sup",
+      C: "I try my best",
+      correct1: "nm"
+  },
+  {
+      question: "How r u",
+      A: "good",
+      B: "bad",
+      C: "great",
+      correct2: "great"
+  }
 ];
 
-console.log(timer);
 //functions
-function startCountdown() {  
-    console.log(timer);
-    interval = setInterval(() => {
-        timer.innerHTML = ("Time: " + time);
-        console.log(time);
-      time--;
-      if (time < 0 ) {
-        clearInterval(interval)
-        timer.innerHTML = "Time: 0";
-        alert('Quiz Over')
-      }
-    }, 1000);
+var restart = function() {
+  location.reload();
+}
 
-  };
+// function selectObjKeys() {
 
-
-
-// var questions = function() {
-//     console.log("hi");
-//     createQuestions();
 // };
 
-var createQuestions = function() {
-    time = startTime;
-    startCountdown();
-    for (var i=0; i<allQuestions.length; i++) {
-        console.log(allQuestions[i]);
+function startCountdown() {  
+  // selectObjKeys();
+  interval = setInterval(() => {
+      timer.innerHTML = ("Time: " + time);
+    time--;
+    if (time < 0 ) {
+      clearInterval(interval)
+      timer.innerHTML = "Time: 0";
+      alert('Quiz Over')
     }
+  }, 1000);
+};
+
+
+var startGame = function() {
+  time = startTime;
+  startCountdown();
+  startBtn.remove();
+  introP.remove();
+  createQuestions();
+
 
 };
 
+var checkanswer = function() {
+  console.log("Hell yeah!");
+  // if
+}
+
+var createQuestions = function() {
+  var buttonA = document.createElement("button");
+  var buttonB = document.createElement("button");
+  var buttonC = document.createElement("button");
+  var A = answersBox.appendChild(buttonA);
+  var B = answersBox.appendChild(buttonB);
+  var C = answersBox.appendChild(buttonC);
+  A.setAttribute("id", "A");
+  B.setAttribute("id", "B");
+  C.setAttribute("id", "C");
+
+  // for (var i=0; i<allQuestions.length; i++) {
+    questionDisplay.innerText = allQuestions[questionCounter].question;
+    buttonA.innerText = allQuestions[questionCounter].A;
+    buttonB.innerText = allQuestions[questionCounter].B;
+    buttonC.innerText = allQuestions[questionCounter].C;
+  // }
+  buttonA.addEventListener("click", checkanswer())
+}
+
 //event listener
-startBtn.addEventListener("click", createQuestions);
+startBtn.addEventListener("click", startGame);
