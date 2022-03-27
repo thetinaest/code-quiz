0a3a8885f2a2e026b255b0507451c9334a399c7a
LittleToes <kristina.r.brennan@gmail.com>
2022-03-26 19:40:10 -0500

    sets score and initials to localstorage, includes highscores page

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 // global vars
 var startBtn = document.querySelector("#start-quiz");
 var startTime = 5;
-//don't forget to set time back to 75 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 var time = startTime;
 var timer = document.querySelector('#count-down');
 var questionCounter = 0;
@@ -27,56 +26,16 @@ var allQuestions = [
 ];
 
 //functions
-var restart = function() {
-  location.reload();
-}
-
-function startCountdown() {  
-  interval = setInterval(() => {
-      timer.innerHTML = ("Time: " + time);
-    time--;
-    if (time < 0 ) {
-      clearInterval(interval)
-      timer.innerHTML = "Time: 0";
-      // alert('Quiz Over')
-    }
-  }, 1000);
-};
-
 
 var startGame = function() {
   time = startTime;
   startCountdown();
   startBtn.remove();
-  introP.remove();
-  // reset();
+  reset();
   createQuestions(questionCounter);
 };
 
-var reset = function() {
-  questionDisplay.innerText = "";
-  var child = answersBox.lastElementChild; 
-  while (child) {
-      answersBox.removeChild(child);
-      child = answersBox.lastElementChild;
-  };
-  // createQuestions();
-}
-
-var checkanswer = function(event) {
-  if (event.target.innerText === allQuestions[questionCounter].correct) {
-    answerCheck.innerText = "Correct!"
-  } else {
-    answerCheck.innerText = "Incorrect! -10 seconds."
-  }
-  questionCounter++;
-  console.log(questionCounter);
-  reset();
-  createQuestions();
-
-}
-
-var createQuestions = function(questionCounter) {
+var createQuestions = function() {
   questionDisplay.innerText = allQuestions[questionCounter].question;
   var buttonA = document.createElement("button");
   var buttonB = document.createElement("button");
@@ -90,12 +49,118 @@ var createQuestions = function(questionCounter) {
   A.setAttribute("id", "A");
   B.setAttribute("id", "B");
   C.setAttribute("id", "C");
-
-
   document.querySelector("#A").addEventListener("click", checkanswer)
   document.querySelector("#B").addEventListener("click", checkanswer)
   document.querySelector("#C").addEventListener("click", checkanswer)
 }
+
+function startCountdown() {  
+  interval = setInterval(() => {
+      timer.innerHTML = ("Time: " + time);
+    time--;
+    if (time < 0) {
+      time = 0;
+      endQuiz();
+      return;
+    }
+  }, 1000);
+};
+
+var checkanswer = function(event) {
+  if (event.target.innerText === allQuestions[questionCounter].correct) {
+    answerCheck.innerText = "Correct!"
+  } else {
+    time-=10;
+    answerCheck.innerText = "Incorrect! -10 seconds."
+  }
+  if (time < 0) {
+    time = 0;
+    endQuiz();
+    return;
+  }
+
+  questionCounter++;
+  console.log(questionCounter);
+  reset();
+  if (questionCounter >= allQuestions.length) {
+    console.log("why");
+    console.log(allQuestions.length);
+    endQuiz();
+    return;
+  } else {
+    createQuestions();
+  }
+
+}
+
+var reset = function() {
+  questionDisplay.innerText = "";
+  var child = answersBox.lastElementChild; 
+  while (child) {
+      answersBox.removeChild(child);
+      child = answersBox.lastElementChild;
+  };
+}
+
+var endQuiz = function() {
+  clearInterval(interval);
+  reset();
+  questionDisplay.innerText = "Quiz complete!"
+  answersBox.innerText = "Quiz over!";
+  var endingNote = document.createElement("p");
+  endingNote.innerText = "Your score is " + time + "! Please enter your initials to save your highscore!";
+  answersBox.appendChild(endingNote);
+
+  var saveInitials = document.createElement("input");
+  saveInitials.setAttribute("type", "text");
+  answersBox.appendChild(saveInitials);
+  saveInitials.setAttribute("name", "initials");
+
+  var endSubmit = document.createElement("button");
+  endSubmit.innerText = "Submit";
+  endSubmit.setAttribute("type", "submit");
+  answersBox.appendChild(endSubmit);
+  endSubmit.addEventListener("click", submitScore);
+  // let them save high scores
+  // ask if they want to play again
+  //   if yes restart
+  //   if no, nothing
+}
+
+var submitScore = function(event) {
+  event.preventDefault();
+  var initials = document.querySelector("input[name='initials']").value;
+  console.log(initials);
+  if (!initials) {
+    alert("You must enter your initials to save your score. Please try again.");
+    return;
+  }
+  
+  var testScores = localStorage.getItem("testHS");
+  if (!testScores) {
+    localStorage.setItem("testHS", JSON.stringify([{initials:initials, score:time}]));
+    location.assign("highscores.html");
+    return;
+  }
+  // testScores
+
+  // //   if (!highScores) {
+  //     localStorage.setItem("QuizhighScores", JSON.stringify([{initials:initials, score:time}]));
+  // //     location.assign("highscores.html");
+  // //     return;
+  // //   }
+  // // var highScores = localstorage.getItem("highScores");
+  //   highScores = JSON.parse(highScores);
+  //   highScores.push({initials: initials, score:time});
+  //   localStorage.setItem("highScores", JSON.stringify(highScores));
+  //   location.assign("highscores.html");
+}
+
+
+// var restart = function() {
+//   location.reload();
+// }
+
 
 //event listeners
 startBtn.addEventListener("click", startGame);
